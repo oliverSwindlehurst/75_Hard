73e7cc555d08ba97078d812e5e36bde141b5adb6
oliverSwindlehurst <50767261+oliverSwindlehurst@users.noreply.github.com>
2020-10-15 16:57:36 +0100

    First Commit
    Commit to copy the solution and files to thr repository

---
+++ 75Hard/75Hard/App.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace _75Hard
+{
+    public partial class App : Application
+    {
+        private const string Exercise_1Key = "Excercise_1";
+        private const string Exercise_2Key = "Excercise_2";
+        private const string DietKey = "Diet";
+        private const string StudyKey = "Study";
+        private const string Water_1Key = "Water_1";
+        private const string Water_2Key = "Water_2";
+        private const string Water_3Key = "Water_3";
+        private const string Water_4Key = "Water_4";
+        private const string Book_ReadKey = "Book_Read";
+        private const string Progress_PictureKey = "Progress_Picture";
+        private const string Day_CounterKey = "0";
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+        public bool Exercise_1
+        {
+            get
+            {
+                if (Properties.ContainsKey(Exercise_1Key))
+                {
+                    return (bool)Properties[Exercise_1Key];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Exercise_1Key] = value;
+            }
+        }
+
+        public bool Exercise_2
+        {
+            get
+            {
+                if (Properties.ContainsKey(Exercise_2Key))
+                {
+                    return (bool)Properties[Exercise_2Key];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Exercise_2Key] = value;
+            }
+        }
+
+        public bool Diet
+        {
+            get
+            {
+                if (Properties.ContainsKey(DietKey))
+                {
+                    return (bool)Properties[DietKey];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[DietKey] = value;
+            }
+        }
+
+        public bool Study
+        {
+            get
+            {
+                if (Properties.ContainsKey(StudyKey))
+                {
+                    return (bool)Properties[StudyKey];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[StudyKey] = value;
+            }
+        }
+
+        public bool Water_1
+        {
+            get
+            {
+                if (Properties.ContainsKey(Water_1Key))
+                {
+                    return (bool)Properties[Water_1Key];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Water_1Key] = value;
+            }
+        }
+
+        public bool Water_2
+        {
+            get
+            {
+                if (Properties.ContainsKey(Water_2Key))
+                {
+                    return (bool)Properties[Water_2Key];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Water_2Key] = value;
+            }
+        }
+
+        public bool Water_3
+        {
+            get
+            {
+                if (Properties.ContainsKey(Water_3Key))
+                {
+                    return (bool)Properties[Water_3Key];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Water_3Key] = value;
+            }
+        }
+
+        public bool Water_4
+        {
+            get
+            {
+                if (Properties.ContainsKey(Water_4Key))
+                {
+                    return (bool)Properties[Water_4Key];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Water_4Key] = value;
+            }
+        }
+
+        public bool Book_Read
+        {
+            get
+            {
+                if (Properties.ContainsKey(Book_ReadKey))
+                {
+                    return (bool)Properties[Book_ReadKey];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Book_ReadKey] = value;
+            }
+        }
+
+        public bool Progress_Picture
+        {
+            get
+            {
+                if (Properties.ContainsKey(Progress_PictureKey))
+                {
+                    return (bool)Properties[Progress_PictureKey];
+                }
+                return false;
+            }
+            set
+            {
+                Properties[Progress_PictureKey] = value;
+            }
+        }
+
+        public int Day_Counter
+        {
+            get
+            {
+                if (Properties.ContainsKey(Day_CounterKey))
+                {
+                    return Convert.ToInt32(Properties[Day_CounterKey]);
+                }
+                return 0;
+            }
+            set
+            {
+                Properties[Day_CounterKey] = value;
+            }
+        }
+    }
+}
---
+++ 75Hard/75Hard/MainPage.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace _75Hard
+{
+    public partial class MainPage : ContentPage
+    {
+        int counter;
+        int tally;
+        public MainPage()
+        {
+            InitializeComponent();
+            StartupCheck();
+        }
+
+        private void StartupCheck()
+        {
+            var app = Application.Current as App;
+
+            cb_Exercise_1.IsChecked = app.Exercise_1;
+            cb_Exercise_2.IsChecked = app.Exercise_2;
+            cb_Diet.IsChecked = app.Diet;
+            cb_Study.IsChecked = app.Study;
+            cb_Water_1.IsChecked = app.Water_1;
+            cb_Water_2.IsChecked = app.Water_2;
+            cb_Water_3.IsChecked = app.Water_3;
+            cb_Water_4.IsChecked = app.Water_4;
+            cb_Book_Read.IsChecked = app.Book_Read;
+            cb_Progress_Picture.IsChecked = app.Progress_Picture;
+            Day_Counter.Text = "Day Counter: " + app.Day_Counter;
+        }
+
+        private void Button_Clicked_Day_Done(object sender, EventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (tally == 10)
+            {
+                if (counter == 75)
+                {
+                    counter = 0;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            else
+            {
+                counter = 0;
+                tally = 0;
+            }
+
+            app.Day_Counter = counter;
+
+            Uncheck_All();
+
+            Day_Counter.Text = "Day Counter: " + counter;
+        }
+        private void Uncheck_All()
+        {
+            cb_Exercise_1.IsChecked = false;
+            cb_Exercise_2.IsChecked = false;
+            cb_Diet.IsChecked = false;
+            cb_Study.IsChecked = false;
+            cb_Water_1.IsChecked = false;
+            cb_Water_2.IsChecked = false;
+            cb_Water_3.IsChecked = false;
+            cb_Water_4.IsChecked = false;
+            cb_Book_Read.IsChecked = false;
+            cb_Progress_Picture.IsChecked = false;
+        }
+        private void Button_Clicked_Check_All(object sender, EventArgs e)
+        {
+            cb_Exercise_1.IsChecked = true;
+            cb_Exercise_2.IsChecked = true;
+            cb_Diet.IsChecked = true;
+            cb_Study.IsChecked = true;
+            cb_Water_1.IsChecked = true;
+            cb_Water_2.IsChecked = true;
+            cb_Water_3.IsChecked = true;
+            cb_Water_4.IsChecked = true;
+            cb_Book_Read.IsChecked = true;
+            cb_Progress_Picture.IsChecked = true;
+        }
+        private void CheckedChanged_1(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if(cb_Exercise_1.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Exercise_1 = cb_Exercise_1.IsChecked;
+        }
+        private void CheckedChanged_2(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Exercise_2.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Exercise_2 = cb_Exercise_2.IsChecked;
+        }
+        private void CheckedChanged_3(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Diet.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Diet = cb_Diet.IsChecked;
+        }
+        private void CheckedChanged_4(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Study.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Study = cb_Study.IsChecked;
+        }
+        private void CheckedChanged_5(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Water_1.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Water_1 = cb_Water_1.IsChecked;
+        }
+        private void CheckedChanged_6(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Water_2.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Water_2 = cb_Water_2.IsChecked;
+        }
+        private void CheckedChanged_7(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Water_3.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Water_3 = cb_Water_3.IsChecked;
+        }
+        private void CheckedChanged_8(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Water_4.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Water_4 = cb_Water_4.IsChecked;
+        }
+        private void CheckedChanged_9(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Book_Read.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Book_Read = cb_Book_Read.IsChecked;
+        }
+        private void CheckedChanged_10(object sender, CheckedChangedEventArgs e)
+        {
+            var app = Application.Current as App;
+
+            if (cb_Progress_Picture.IsChecked == true)
+            {
+                tally++;
+            }
+            else
+            {
+                tally--;
+            }
+
+            app.Progress_Picture = cb_Progress_Picture.IsChecked;
+        }
+    }
+}
